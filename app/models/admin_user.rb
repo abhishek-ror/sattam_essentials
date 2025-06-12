25dc734028e4267e1289380e758423f966f41e40
Abhishek Pal <apal51640@gmail.com>
2025-06-12 10:45:18 +0530

    create adin console and set role superadmin and admin

---
+++ app/models/admin_user.rb
@@ -0,0 +1,10 @@
+class AdminUser < ApplicationRecord
+  devise :database_authenticatable,
+         :recoverable, :rememberable, :validatable
+
+	enum role: { admin: 'admin', superadmin: 'superadmin' }
+
+  def self.ransackable_attributes(auth_object = nil)
+    %w[id email role created_at updated_at]
+  end
+end
